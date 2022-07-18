e833d082ba3a826e073797573050c887b344d5ce
Taemin Kim <taeminkimny@gmail.com>
2022-07-18 11:49:23 -0400

    commit tree 6d3c59a05de9989cd2ac82646010657aa6850fac

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = 
+    if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    render json: students
+  end
+
+  def show
+    students = Student.find_by(id: params[:id])
     render json: students
   end
 
